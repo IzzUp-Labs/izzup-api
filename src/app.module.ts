4eb112f5c3ad940b1894fdbdeafd97ed0526fe33
DaFonsecaDamien <da.fonseca.damien@gmail.com>
2023-03-30 15:45:11 +0200

    :wrench: [UPD] : Best practice with nestjs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import databaseConfig from "./infrastructure/config/database.config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import appConfig from "./infrastructure/config/app.config";
+import { TypeOrmConfigService } from "./infrastructure/config/typeorm-config.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { configService } from "./infrastructure/config/typeorm/config.service";
+import { UserModule } from "./infrastructure/modules/user.module";
+import { AuthModule } from "./infrastructure/modules/auth.module";
+import authConfig from "./infrastructure/config/auth.config";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        databaseConfig,
+        appConfig,
+        authConfig,
+      ],
+      envFilePath: ['env/local.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+      inject: [ConfigService],
+    }),
+    UserModule,
+    AuthModule
+  ],
   controllers: [],
   providers: [],
 })
