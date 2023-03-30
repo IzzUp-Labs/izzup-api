9e5f0ee7c6892861c3eaa734346b144ed359c86f
DaFonsecaDamien <da.fonseca.damien@gmail.com>
2023-03-30 15:54:28 +0200

    :technologist: [ADD] : App versioning, globalPrefix and Cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as process from "process";
+import { ConfigService } from "@nestjs/config";
+import { VersioningType } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const configService = app.get(ConfigService);
+  app.setGlobalPrefix(configService.get('app.apiPrefix'), {
+    exclude: ['/'],
+  });
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  await app.listen(configService.get('app.port'));
 }
 bootstrap();
+
