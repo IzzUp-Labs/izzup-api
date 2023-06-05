73f01925782e939625318880c499da338401efb4
Damien <da.fonseca.damien@gmail.com>
2023-06-05 12:32:02 +0200

    :wrench: [ADD] Config file to hide admin account information in the seeder

--- src/infrastructure/database/seeds/user/user-seed.service.ts
+++ src/infrastructure/database/seeds/user/user-seed.service.ts
@@ -3,12 +3,15 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { UserEntity } from "../../../entities/user.entity";
 import { RoleEnum } from "../../../../domain/utils/enums/role.enum";
+import { ConfigService } from "@nestjs/config";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UserSeedService {
   constructor(
     @InjectRepository(UserEntity)
     private repository: Repository<UserEntity>,
+    private configService: ConfigService
   ) {}
 
   async run() {
@@ -19,12 +22,13 @@ export class UserSeedService {
     })
 
     if (countAdmin === 0) {
+      const hashedPassword = await bcrypt.hash(this.configService.get("seed-user.seed_admin_password"), 10);
       await this.repository.save(
         this.repository.create({
-          first_name: 'Super',
-          last_name: 'Admin',
-          email: 'admin@example.com',
-          password: 'secret',
+          first_name: this.configService.get("seed-user.seed_admin_firstname"),
+          last_name: this.configService.get("seed-user.seed_admin_lastname"),
+          email: this.configService.get("seed-user.seed_admin_email"),
+          password: hashedPassword,
           role: RoleEnum.ADMIN,
         }),
       );
