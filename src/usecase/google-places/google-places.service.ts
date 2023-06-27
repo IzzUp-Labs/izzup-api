cafdd20c2cbfde9cdc82ee69e58b07ee7effc8a3
Damien Da Fonseca <da.fonseca.damien@gmail.com>
2023-06-27 23:06:18 +0200

    :bug: [FIX] .env.example with the google api key and fix config in google places service

--- src/usecase/google-places/google-places.service.ts
+++ src/usecase/google-places/google-places.service.ts
@@ -14,7 +14,7 @@ export class GooglePlacesService {
     async findPlaceFromText(nameOrAddress: string){
         const fields = 'formatted_address%2Cname%2Cplace_id%2Ctype%2Cuser_ratings_total%2Crating%2Cgeometry'
         const {data} = await firstValueFrom(
-            this.httpService.get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=${fields}&input=${nameOrAddress}&inputtype=textquery&key=${this.configService.get('google_api_key')}`).pipe(
+            this.httpService.get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=${fields}&input=${nameOrAddress}&inputtype=textquery&key=${this.configService.get('google-api.google_api_key')}`).pipe(
                 catchError((error: AxiosError) => {
                     throw new HttpException('Company not found with this address or name\n'+error, 404);
                 }),
@@ -26,7 +26,7 @@ export class GooglePlacesService {
     async getPlaceDetails(placeId: string){
         const fields = 'place_id%2Cuser_ratings_total%2Crating%2Cgeometry%2Cformatted_phone_number%2Cformatted_address%2Cname%2Copening_hours%2Cwebsite%2Curl%2Cvicinity%2Ctypes%2Cphotos%2Cadr_address%2Cbusiness_status%2Cicon%2Cinternational_phone_number%2Cplus_code%2Cprice_level%2Creviews%2Cscope';
         const {data} = await firstValueFrom(
-            this.httpService.get(`https://maps.googleapis.com/maps/api/place/details/json?fields=${fields}&place_id=${placeId}&key=${this.configService.get('google_api_key')}`).pipe(
+            this.httpService.get(`https://maps.googleapis.com/maps/api/place/details/json?fields=${fields}&place_id=${placeId}&key=${this.configService.get('google-api.google_api_key')}`).pipe(
                 catchError((error: AxiosError) => {
                     throw new HttpException('Company not found with this address or name\n'+error, 404);
                 }),
