bc578dcf0fa70fd9003c6160c19afaab7710bd0a
Damien Da Fonseca <da.fonseca.damien@gmail.com>
2023-06-30 13:24:42 +0200

    :white_check_mark: [UPD] Tests according to change

--- test/e2e/extra/extra.controller.spec.ts
+++ test/e2e/extra/extra.controller.spec.ts
@@ -3,6 +3,8 @@ import { ExtraService } from "../../../src/usecase/extra/extra.service";
 import { getRepositoryToken } from "@nestjs/typeorm";
 import { ExtraEntity } from "../../../src/usecase/extra/entities/extra.entity";
 import { ExtraController } from "../../../src/usecase/extra/extra.contoller";
+import {ParamCheckService} from "../../../src/domain/middleware/param-check/param-check.service";
+import {JwtService} from "@nestjs/jwt";
 
 describe('ExtraController', () => {
   let controller: ExtraController;
@@ -11,11 +13,14 @@ describe('ExtraController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ExtraController],
       providers: [
+        JwtService,
+        ParamCheckService,
         ExtraService,
         {
           provide: getRepositoryToken(ExtraEntity),
           useValue: ExtraEntity
-        }
+        },
+
       ],
     }).compile();
 
