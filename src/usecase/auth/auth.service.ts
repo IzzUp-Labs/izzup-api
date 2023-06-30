a668b284881ee7c5e445ce57fe7b93d890278e8f
Damien Da Fonseca <da.fonseca.damien@gmail.com>
2023-06-30 13:11:19 +0200

    :bug: [FIX] Email in lowercase

--- src/usecase/auth/auth.service.ts
+++ src/usecase/auth/auth.service.ts
@@ -67,7 +67,7 @@ export class AuthService {
 
   async isMember(authMembershipCheckDto: AuthMembershipCheckDto): Promise<boolean> {
     const user = await this.userService.findOne({
-      email: authMembershipCheckDto.email
+      email: authMembershipCheckDto.email.toLowerCase(),
     });
     return !!user;
   }
@@ -79,6 +79,7 @@ export class AuthService {
     });
     return await this.userService.create({
       ...authRegisterExtraDto,
+      email: authRegisterExtraDto.email.toLowerCase(),
       password: hashedPassword,
       role: RoleEnum.EXTRA,
       extra: extra,
@@ -93,6 +94,7 @@ export class AuthService {
     const employer = await this.employerService.create({});
     const user = await this.userService.create({
         ...authRegisterEmployer,
+        email: authRegisterEmployer.email.toLowerCase(),
         password: hashedPassword,
         role: RoleEnum.EMPLOYER,
         employer: employer
