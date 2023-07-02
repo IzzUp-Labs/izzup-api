7f6ac316f8e6cc2e823de224ffb54e592b1ad3bc
Damien Da Fonseca <da.fonseca.damien@gmail.com>
2023-07-02 17:42:48 +0200

    :ambulance: [FIX] Critical fix on entity and fix test

--- src/usecase/extra/extra.module.ts
+++ src/usecase/extra/extra.module.ts
@@ -5,14 +5,9 @@ import { ExtraController } from "./extra.contoller";
 import { ExtraService } from "./extra.service";
 import { JobOfferModule } from "../job-offer/job-offer.module";
 import { ParamCheckModule } from "../../domain/middleware/param-check/param-check.module";
-import {ExtraJobRequestModule} from "./extra-job-request.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ExtraEntity]),
-    JobOfferModule,
-    ParamCheckModule,
-    ExtraJobRequestModule
-  ],
+  imports: [TypeOrmModule.forFeature([ExtraEntity]), JobOfferModule, ParamCheckModule],
   controllers: [ExtraController],
   providers: [ExtraService],
   exports: [ExtraService]
