a2a1b2fdba52c27879a62cfd367c48c1d9c928fd
Damien Da Fonseca <da.fonseca.damien@gmail.com>
2023-10-02 15:42:31 +0200

    :white_check_mark: [FIX] Google places tests

--- test/e2e/google-places/google-places.controller.spec.ts
+++ test/e2e/google-places/google-places.controller.spec.ts
@@ -3,6 +3,9 @@ import { GooglePlacesController } from "../../../src/usecase/google-places/googl
 import { GooglePlacesService } from "../../../src/usecase/google-places/google-places.service";
 import { HttpService } from "@nestjs/axios";
 import { ConfigService } from "@nestjs/config";
+import {CompanyService} from "../../../src/usecase/company/company.service";
+import {CompanyEntity} from "../../../src/usecase/company/entities/company.entity";
+import {getRepositoryToken} from "@nestjs/typeorm";
 
 describe("GooglePlacesController", () => {
   let controller: GooglePlacesController;
@@ -10,10 +13,14 @@ describe("GooglePlacesController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GooglePlacesController],
-      providers: [GooglePlacesService, HttpService, ConfigService,
+      providers: [GooglePlacesService, HttpService, ConfigService,CompanyService,
         {
           provide: "AXIOS_INSTANCE_TOKEN",
           useValue: "AXIOS_INSTANCE_TOKEN"
+        },
+        {
+          provide: getRepositoryToken(CompanyEntity),
+          useValue: CompanyEntity
         }]
     }).compile();
 
--- test/unit/google-places/google-places.service.spec.ts
+++ test/unit/google-places/google-places.service.spec.ts
@@ -2,16 +2,23 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { GooglePlacesService } from "../../../src/usecase/google-places/google-places.service";
 import { HttpService } from "@nestjs/axios";
 import { ConfigService } from "@nestjs/config";
+import {CompanyService} from "../../../src/usecase/company/company.service";
+import {CompanyEntity} from "../../../src/usecase/company/entities/company.entity";
+import {getRepositoryToken} from "@nestjs/typeorm";
 
 describe("GooglePlacesService", () => {
   let service: GooglePlacesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GooglePlacesService, HttpService, ConfigService,
+      providers: [GooglePlacesService, HttpService, ConfigService,CompanyService,
         {
           provide: "AXIOS_INSTANCE_TOKEN",
           useValue: "AXIOS_INSTANCE_TOKEN"
+        },
+        {
+          provide: getRepositoryToken(CompanyEntity),
+          useValue: CompanyEntity
         }]
     }).compile();
 
