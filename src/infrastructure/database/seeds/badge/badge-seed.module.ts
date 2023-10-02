863daf85d0b74745e7593e78629fe7d7d21f33a6
Damien Da Fonseca <da.fonseca.damien@gmail.com>
2023-10-02 23:40:27 +0200

    :bug: [FIX] Seeding module for badge

--- src/infrastructure/database/seeds/badge/badge-seed.module.ts
+++ src/infrastructure/database/seeds/badge/badge-seed.module.ts
@@ -2,9 +2,14 @@ import { Module } from "@nestjs/common";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {BadgeEntity} from "../../../../usecase/rating/entities/badge.entity";
 import {BadgeSeedService} from "./badge-seed.service";
+import {BadgeRatingEntity} from "../../../../usecase/rating/entities/badge-rating.entity";
+import {RatingEntity} from "../../../../usecase/rating/entities/rating.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([BadgeEntity])],
+    imports: [TypeOrmModule.forFeature([BadgeEntity]),
+        TypeOrmModule.forFeature([BadgeRatingEntity]),
+        TypeOrmModule.forFeature([RatingEntity])
+    ],
     providers: [BadgeSeedService],
     exports: [BadgeSeedService]
 })
