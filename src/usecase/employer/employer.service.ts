6ce3c0a88bfa6b691b5f65d6dd0b3eb70f60ed59
Damien Da Fonseca <da.fonseca.damien@gmail.com>
2023-10-16 13:09:50 +0200

    :ambulance: [FIX] Adding user_id in notification "job-request-finished"

--- src/usecase/employer/employer.service.ts
+++ src/usecase/employer/employer.service.ts
@@ -252,6 +252,7 @@ export class EmployerService {
     // NOTIFICATION : SEND NOTIFICATION TO EXTRA (JOB-REQUEST-FINISHED)
     await this.notificationService.sendJobNotificationToUser(request.extra.user.id, "job-request-finished-body", {
       type: "job-request-finished",
+      user_id: request.extra.user.id
     });
   }
 
